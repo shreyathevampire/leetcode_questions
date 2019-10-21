befa8a860fd9f1439b1953b3d899cee8fa21008b
Shreya Vanga <31374151+shreyathevampire@users.noreply.github.com>
2019-10-21 10:57:45 +0530

    Create reverse_words_in_a_given_string.cpp

---
+++ reverse_words_in_a_given_string.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+  string s = "geeks for geek";
+  char buf[1024];
+  memset(buf,'\0',1024);
+  strcpy(buf,s.c_str());
+  cout<<"buf = "<<buf<<endl;
+  char *token[1024];
+  int k=0;
+  token[k++] = strtok(buf," ");
+  while((token[k] = strtok(NULL," ")) != NULL){
+    k++;
+  }
+  token[k] = NULL;
+  // for(int i=0;i<k;i++){
+  //   cout<<token[i] <<" ";
+  // }
+  for(int i=0,j=k-1;i<j;i++,j--){
+    swap(token[i],token[j]);
+  }
+  for(int i=0;i<k;i++){
+    cout<<token[i] <<" ";
+  }
+
+  return 0;
+}
